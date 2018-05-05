837a7e02121d8dd0f459440fbd57db925708fc40
fand <fand.gmork@gmail.com>
2018-05-05 23:20:30 +0900

    step 5-3

--- test/EvalSpec.hs
+++ test/EvalSpec.hs
@@ -54,6 +54,8 @@ spec =
       re "(|| #t #t)" `shouldBe` Bool True
       re "(|| #t #f)" `shouldBe` Bool True
       re "(|| #f #f)" `shouldBe` Bool False
+
+    it "string?" $ do
       re "(string=? \"a\" \"a\")" `shouldBe` Bool True
       re "(string=? \"a\" \"b\")" `shouldBe` Bool False
       re "(string<? \"a\" \"a\")" `shouldBe` Bool False
@@ -65,8 +67,26 @@ spec =
       re "(string>=? \"a\" \"b\")" `shouldBe` Bool False
       re "(string>=? \"b\" \"b\")" `shouldBe` Bool True
 
+    it "if" $ do
       re "(if (> 2 3) \"foo\" \"bar\")" `shouldBe` String "bar"
       re "(if (= 3 3) (+ 2 3 (- 5 1)) \"unequal\")" `shouldBe` Number 9
+
+    it "car, cdr" $ do
+      re "(cdr '(a simple test))" `shouldBe` List [Atom "simple", Atom "test"]
+      re "(car (cdr '(a simple test)))" `shouldBe` Atom "simple"
+      re "(car '((this is) a test))" `shouldBe` List [Atom "this", Atom "is"]
+      re "(cons '(this is) 'test)" `shouldBe` DottedList [List [Atom "this", Atom "is"]] (Atom "test")
+      re "(cons '(this is) '())" `shouldBe` List [List [Atom "this", Atom "is"]]
+
+    it "eqv?" $ do
+      re "(eqv? 1 3)" `shouldBe` Bool False
+      re "(eqv? 3 3)" `shouldBe` Bool True
+      re "(eqv? 'atom 'atom)" `shouldBe` Bool True
+
+    it "equal?" $ do
+      re "(equal? 123 123)" `shouldBe` Bool True
+      re "(equal? 123 \"123\")" `shouldBe` Bool True
+      re "(equal? 123 \"foo\")" `shouldBe` Bool False
 
     it "throws" $ do
       le "(symbol? 1 2)" `shouldStartWith` "Expected 1 args;"
