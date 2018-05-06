cf48743fd8bca795fc23240c6601408457684c55
fand <fand.gmork@gmail.com>
2018-05-06 14:01:29 +0900

    step 6

--- app/Main.hs
+++ app/Main.hs
@@ -3,11 +3,37 @@ module Main where
 import           Eval
 import           Read
 import           System.Environment
+import           System.IO
 import           Types
 
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr =
+  let evaled = fmap show $ readExpr expr >>= eval in
+  return $ extractValue $ trapError evaled
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+    then return ()
+    else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
+
 main :: IO ()
--- main = getArgs >>= print . eval . readExpr . head
 main = do
   args <- getArgs
-  evaled <- return $ fmap show $ readExpr (head args) >>= eval
-  putStrLn $ extractValue $ trapError evaled
+  case length args of
+    0 -> runRepl
+    1 -> evalAndPrint $ head args
+    _ -> putStrLn "Program takes only 0 or 1 argument"
